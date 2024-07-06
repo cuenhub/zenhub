600c57058286b16389590a5b52c0530930879311
cuen <136510780+cuenhub@users.noreply.github.com>
2024-07-06 10:39:34 +0800

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,1 +1,13 @@
+local supportedgames = {
+   3840352284,
+}
 
+
+for _, id in ipairs(supportedgames) do
+        if game.PlaceId == id then
+            print("Hello World")
+            return
+  else
+    
+        end
+ end
